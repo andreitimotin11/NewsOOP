f09970bf22db1730c9dbdd530eac84455775b34d
Andrei Timotin <andreitimotin11@gmail.com>
2016-12-06 20:46:32 +0200

    add autoload

--- www/models/news.php
+++ www/models/news.php
@@ -14,29 +14,10 @@ class News{
 	public $text;
 	public static function getAll(){
 		$db = new DB;
-		return $db->query("SELECT * FROM news", "News");
+		return $db->queryAll("SELECT * FROM news", "News");
+	}
+	public static function getOne($id){
+		$db =  new DB();
+		return $db->queryOne('SELECT * FROM news WHERE id = ' . $id, 'News');
 	}
 }
-/*
-class DataBase{
-	public $host = "127.0.0.1";
-	public $user = "root";
-	public $pass = "";
-	public $db = "GB_News";
-	function __construct(){
-		mysql_connect('localhost', 'root', '');
-		mysql_select_db('GB_News');
-	}
-	function query($sql){
-		return mysql_query($sql);
-	}
-
-}
-function newsGetAll(){
-	$db  =  new DataBase();
-	$sql = "SELECT * FROM news";
-	$res = $db->query($sql);
-	$items = mysql_fetch_assoc($res);
-	return $items;
-}
-*/
