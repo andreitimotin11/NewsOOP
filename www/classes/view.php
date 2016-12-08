ef8378a4736c93d67f5556136a4cdaa35670af2b
Andrei Timotin <andreitimotin11@gmail.com>
2016-12-09 00:55:03 +0200

    add View.php

---
+++ www/classes/view.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Andrei
+ * Date: 09.12.2016
+ * Time: 00:06
+ */
+class View
+{
+	protected $data = array();
+	public function assign($name, $value)
+	{
+		$this->data[$name] = $value;
+	}
+	public function __set($k, $v)
+	{
+		$this->data[$k] = $v;
+	}
+
+	public function display($template){
+		foreach ($this->data as $key => $val)
+		{
+			$$key = $val;
+		}
+		include __DIR__ . "/../views/" . $template;
+	}
+}
