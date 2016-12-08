cb6daf8ae0d2b95e0d5eaab0355b830f7700cb11
Andrei Timotin <andreitimotin11@gmail.com>
2016-12-08 17:43:16 +0200

    add AbstractModel

--- www/classes/AbstractModel.php
+++ www/classes/AbstractModel.php
@@ -9,9 +9,17 @@
 abstract class AbstractModel
 {
 	protected static $table;
+	protected static $class;
+
 	public static function getAll(){
 		$db = new DB;
-		return $db->queryAll("SELECT * FROM " . self::$table . ", News");
-		
+		$sql = "SELECT * FROM " . static::$table;
+		return $db->queryAll($sql, static::$class);
+
+	}
+	public static function getOne($id){
+		$db =  new DB();
+		$sql = 'SELECT * FROM ' . static::$table . 'WHERE id = ' . $id ;
+		return $db->queryOne($sql,static::$class);
 	}
 }
