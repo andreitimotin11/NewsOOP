811a2708df3f0926340cc7a3993f74796900ef6f
Andrei Timotin <andreitimotin11@gmail.com>
2016-12-16 19:47:44 +0200

    Lucram cu Buferizarea

--- www/classes/view.php
+++ www/classes/view.php
@@ -18,23 +18,21 @@ implements Countable
 	{
 		return $this->data[$k];
 	}
-	public function display($template){
+	public function render($template){
 		foreach ($this->data as $key => $val)
 		{
 			$$key = $val;
 		}
+		ob_start();
 		include __DIR__ . "/../views/" . $template;
+		$content = ob_get_contents();
+		ob_end_clean();
+		return $content;
+	}
+	public function display($template){
+		echo $this->render($template);
 	}
 
-	/**
-	 * Count elements of an object
-	 * @link http://php.net/manual/en/countable.count.php
-	 * @return int The custom count as an integer.
-	 * </p>
-	 * <p>
-	 * The return value is cast to an integer.
-	 * @since 5.1.0
-	 */
 	public function count()
 	{
 		// TODO: Implement count() method.
