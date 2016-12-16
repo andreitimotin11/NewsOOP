e88f5cfdff79deff2279612c522a7068702dca4c
Andrei Timotin <andreitimotin11@gmail.com>
2016-12-16 19:29:05 +0200

    Incepem Buferizarea

--- www/classes/view.php
+++ www/classes/view.php
@@ -7,18 +7,37 @@
  * Time: 00:06
  */
 class View
+implements Countable
 {
 	protected $data = array();
 	public function __set($k, $v)
 	{
 		$this->data[$k] = $v;
 	}
-
+	public function __get($k)
+	{
+		return $this->data[$k];
+	}
 	public function display($template){
 		foreach ($this->data as $key => $val)
 		{
 			$$key = $val;
 		}
 		include __DIR__ . "/../views/" . $template;
+	}
+
+	/**
+	 * Count elements of an object
+	 * @link http://php.net/manual/en/countable.count.php
+	 * @return int The custom count as an integer.
+	 * </p>
+	 * <p>
+	 * The return value is cast to an integer.
+	 * @since 5.1.0
+	 */
+	public function count()
+	{
+		// TODO: Implement count() method.
+		return count($this->data);
 	}
 }
