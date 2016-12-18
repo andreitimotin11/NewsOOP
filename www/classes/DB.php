74e22a647019d77ab1f4cac1b9dde1e012f038c6
Andrei Timotin <andreitimotin11@gmail.com>
2016-12-18 21:43:43 +0200

    TEMA 5. PDO. modified AbstractModel

--- www/classes/DB.php
+++ www/classes/DB.php
@@ -4,15 +4,20 @@ class DB
 {
 	private $dbh;
 	private $className = 'stdClass';
+
 	public function __construct()
 	{
 
-		$this->dbh = new PDO("mysql:dbname=GB_News;host=localhost", "root","");
+		$this->dbh = new PDO("mysql:dbname=GB_News;host=localhost", "root", "");
 	}
-	public function setClassName($className){
+
+	public function setClassName($className)
+	{
 		$this->className = $className;
 	}
-	public function query($sql, $params = []){
+
+	public function query($sql, $params = [])
+	{
 		$sth = $this->dbh->prepare($sql);
 		$sth->execute($params);
 
